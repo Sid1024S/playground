acfdbfa8c832d9b5fec3072d861ba012fbf534a1
ShyTa <st.sw.tech@gmail.com>
2024-11-11 00:23:30 -0500

    update title and message

--- python/streamlit-demo/app2.py
+++ python/streamlit-demo/app2.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 
 
-st.title("Simple chat")
+st.title("Risk and PnL Analysis Bot")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -46,9 +46,9 @@ if prompt := st.chat_input("What can I help with?") or st.session_state.is_strea
         expander = st.expander("Source")
         expander.write("The data is sourced from Finance Report and Yahoo News")
         left, middle, right = st.columns(3)
-        left.button("Data", icon=":material/dataset:", type="primary", on_click=on_button_click, kwargs={"content":"Data"})
+        left.button("Data", icon=":material/dataset:", type="primary", on_click=on_button_click, kwargs={"content":"Once implemented I will display a data grid here"})
         middle.button("Chart", icon=":material/bar_chart:", type="primary", on_click=on_button_click, kwargs={"content":"Chart"})
-        right.button("Meeting", icon=":material/groups:", on_click=on_button_click, kwargs={"content":"Meeting"})
+        right.button("Meeting", icon=":material/groups:", on_click=on_button_click, kwargs={"content":"Once implemented I will set up a meeting with Risk Managers/ Desk Trader"})
         if(st.session_state.content_to_stream=="Chart"):
             st.bar_chart(np.random.randn(30, 3))
         else:
